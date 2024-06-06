6f9eb13de74056b99971cbec5780a7f8304af072
Tomiwa <codertomiwa@gmail.com>
2024-06-06 20:21:33 +0100

    update to 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,13 +1,4 @@
 window.addEventListener('load', function () {
-  // task 3
-  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
-
   // task 2
   const amenityIds = {};
   $('input[type=checkbox]').click(function () {
